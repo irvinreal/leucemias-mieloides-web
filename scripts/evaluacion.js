b2c163bf7775fd30ef595ec0ff0acc4f3c5d89e1
Irvin Gonzalez Real <irvin.glez.real@hotmail.com>
2023-02-18 18:28:38 -0600

    Tercer actualización

--- scripts/evaluacion.js
+++ scripts/evaluacion.js
@@ -1,31 +1,68 @@
+const resultado = document.querySelector(".resultado");
+const btn_enviar = document.getElementById("btn-enviar");
 const form = document.getElementById("form");
+const myalert = document.querySelector(".alert-info");
+const myalertBtns = document.querySelector(".alert-btns");
+const resultado_respuesta = document.querySelectorAll(".resultado-respuesta");
+const enviando_respuestas = document.querySelector("#enviando-respuestas");
+
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 });
-var resultado = document.querySelector(".resultado");
+
+btn_enviar.addEventListener("click", () => {
+    verificarRespuestas();
+});
+
 function verificarRespuestas() {
-    var total = 1;
+    var total = 4;
     var puntos = 0;
 
     var myForm = document.forms["quizForm"];
-    var respuestas = ["a"];
+    var respuestas = ["a", "a", "b", "b"];
 
-    for (var i = 0; i <= total; i++) {
-        if (myForm["r1"].value === null || myForm["r1"].value === "") {
-            alert("Responde la pregunta "+  i);
+    for (var i = 1; i <= total; i++) {
+        // (myForm["r" + i].value == null || myForm["r" + i].value == "")
+        if (
+            myForm["r1" && "r2" && "r3" && "r4"].value == null ||
+            myForm["r1" && "r2" && "r3" && "r4"].value == ""
+        ) {
+            alert("Completa la evaluación !");
+            location = "#main";
             return false;
         } else {
-            if (myForm["r1"].value === respuestas[i - 1]) {
-                puntos++;
-                resultado.innerHTML = "<h3> Respuesta correcta!</h3>";
-            } else {
-                resultado.innerHTML = "<h3> Respuesta Incorrecta!</h3>";
-            }
+            console.log("ahí vamos");
+            // enviando_respuestas.style.display = "inline";
+            // setTimeout(() => {
+            //     enviando_respuestas.style.display = "none";
+            // }, 1500);
 
+            // setTimeout(() => {
+            if (myForm["r" + i].value === respuestas[i - 1]) {
+                puntos++;
+                location = "#main";
+                console.log(i + " bien" + resultado_respuesta.target);
+                resultado_respuesta[i - 1].innerHTML = `
+                                            <h6 class="respuesta-correcta d-inline px-5">Respuesta Correcta ! <i class="bi bi-check-square-fill"></i></h6>
+                `;
+            } else if (myForm["r" + i].value !== respuestas[i - 1]) {
+                resultado_respuesta[i - 1].innerHTML = `
+                <h6 class="respuesta-incorrecta d-inline px-5">Respuesta Incorrecta ! <i class="bi bi-x-square-fill"></i></h6>
+                `;
+                console.log(i + " mal");
+            }
+            // }, 1500);
         }
+        // setTimeout(() => {
+
+        resultado.innerHTML = "Respuestas correctas: " + puntos + " / " + total;
+
+        myalert.classList.add("on");
+        myalertBtns.classList.add("on-btns");
+
+        location = "#main";
+        // }, 1500);
     }
-    // resultado.innerHTML = puntos + "<h3> Respuestas correctas</h3>";
-    
-    console.log(puntos);
+
     return false;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,133 @@
-const a_que_es_la_leucemia = document.querySelector('#a-que-es-la-leucemia')
-const section_que_es_la_leucemia = document.querySelector('#section-que-es-la-leucemia')
-const btn_close_section = document.querySelector('#btn-close-section')
+// const a_que_es_la_leucemia_nav = document.getElementById(
+//     "a-que-es-la-leucemia-nav"
+// );
 
-let animado = document.querySelectorAll('.animado');
+// ------------------------------------------------------------------------------------------------------------
+// const a_que_es_la_leucemia_main = document.getElementById(
+//     "a-que-es-la-leucemia-main"
+// );
+// const section_que_es_la_leucemia = document.querySelector(
+//     "#section-que-es-la-leucemia"
+// );
+// const btn_close_section = document.getElementsByClassName("btn-close-section");
+// ------------------------------------------------------------------------------------------------------------
 
-function mostrarScroll() {
-    let scrollTop = document.documentElement.scrollTop;
-    for (let i = 0; i < animado.length; i++) {
-        let alturaAnimado = animado[i].offsetTop;
-        console.log(alturaAnimado)
-        if (alturaAnimado - 400 < scrollTop) {
-            animado[i].style.opacity = 1;
-        }        
+// a_que_es_la_leucemia_nav.addEventListener("click", function () {
+//     section_que_es_la_leucemia.style.display = "block";
+// });
+
+// ------------------------------------------------------------------------------------------------------------
+// a_que_es_la_leucemia_main.addEventListener("click", function () {
+//     section_que_es_la_leucemia.style.display = "block";
+// });
+
+// btn_close_section.addEventListener("click", function () {
+//     section_que_es_la_leucemia.style.display = "none";
+//     // location = "#main";
+// });
+// ------------------------------------------------------------------------------------------------------------
+
+const contendor_lista_a_main = document.getElementById(
+    "contenedor-lista-a-main"
+);
+
+const btn_close = document.querySelectorAll(".btn-close-section");
+const section = document.querySelectorAll(".section");
+
+contendor_lista_a_main.addEventListener("click", (e) => {
+    if (e.target && e.target.tagName === "A") {
+        // e.target.href.classList.add("activo");
+        // console.log(e);
+        let section = e.target.hash;
+        document.querySelector(section).style.display = "block";
+        // document.querySelector(section).classList.add('aparecer')
+        // const sect = document.querySelector(section)
+        // console.log(sect)
+        // sect.classList.add('aparecer')
     }
-}
+});
 
+btn_close.forEach((element) => {
+    element.addEventListener("click", () => {
+        section.forEach((element) => {
+            element.style.display = "none";
+        });
+    });
+});
 
-    window.addEventListener("scroll", mostrarScroll);
+const ocultarSection = (entradas, observador) => {
+    // console.log('ejecutando..')
+    // console.log(entradas)
+    // console.log(observador)
+    entradas.forEach((entrada) => {
+        if (entrada.isIntersecting) {
+            // console.log("La imagen salió del viewport");
+            console.log(entrada.target);
+            entrada.target.classList.add("aparecer");
+        } else {
+            // console.log("Saliendo..");
+            entrada.target.classList.remove("aparecer");
+            entrada.target.style.display = "none";
+            entrada.target.classList.add("desaparecer");
+        }
+    });
+};
 
-a_que_es_la_leucemia.addEventListener('click', function() {
-    section_que_es_la_leucemia.style.display = 'block'
-})
+const observador = new IntersectionObserver(ocultarSection, {
+    root: null,
+    rootMargin: "0px 0px 0px 0px",
+    threshold: 0.7,
+});
 
-btn_close_section.addEventListener('click', function() {
-    section_que_es_la_leucemia.style.display = 'none'
-})
+section.forEach((element) => {
+    observador.observe(element);
+});
+
+// const clasificacion_lma = [
+//     { nombre: "LAM0" },
+//     { nombre: "LAM1" },
+//     { nombre: "LAM2" },
+//     { nombre: "LAM3" },
+//     { nombre: "LAM4" },
+//     { nombre: "LAM5" },
+//     { nombre: "LAM6" },
+//     { nombre: "LAM7" },
+// ];
+
+const clasificacion_lma = ["LAM0", "LAM1", "LAM2", "LAM3", "LAM4", "LAM5", "LAM6", "LAM7"];
+
+const search = document.querySelector(".form-control");
+const btnSubmit = document.querySelector(".btn-outline-info");
+const form = document.getElementById("form");
+const buscador = document.querySelector(".buscador");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+const filtrar = () => {
+    // buscador.innerHTML = "";
+    // console.log(search.value);
+    const buscarTexto = search.value.toUpperCase();
+    // console.log(buscarTexto)
+    // for (let lma of clasificacion_lma) {
+    //     const nombre = lma.nombre
+    //     console.log(nombre);
+    //     if (buscarTexto == nombre) {
+    //         buscador.innerHTML += `<li class="list-group-item buscando">${nombre}</li>`;
+    //     }
+    // }
+    for (let i = 0; i < clasificacion_lma.length; i++) {
+        const lma = clasificacion_lma[i];
+        if (buscarTexto == lma) {
+            console.log(buscarTexto)
+            console.log(lma)
+            buscador.innerHTML += `<li class="list-group-item buscando">${lma}</li>`;
+        }
+    }
+
+    if ((buscador.innerHTML = "")) {
+        buscador.innerHTML += `<li class="list-group-item buscando">No se ha encontrado resultado</li>`;
+    }
+};
+
+btnSubmit.addEventListener("click", filtrar);
